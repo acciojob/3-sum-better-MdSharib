d4beba20b75e424dfa3fde459c5d649c0cbdc4d1
Md Sharib <109461910+MdSharib@users.noreply.github.com>
2023-06-16 22:24:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function threeSum(arr, target) {
 // write your code here
-  
+  for(let l=0; l<arr.length; l++){
+  target += l;
+  for(let i=0; i<arr.length; i++){
+	  for(let j=i+1; j<arr.length; j++){
+		  for(let k=j+1; k<arr.length; k++){
+			  let sum = arr[i] + arr[j] + arr[k];
+			  if(sum === target){
+				  console.log(sum); 
+			  }
+		  }
+	  }
+  }
+}
+  // console.log() "not found";
 }
 
 module.exports = threeSum;
